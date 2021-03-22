2d75fefff4551ce9811c8f7d0fb8346857469f2e
webdev0101 <hesimiao2019@gmail.com>
2021-03-22 20:15:47 +0800

    added invite box

--- imports/ui/components/lm-invite/component.jsx
+++ imports/ui/components/lm-invite/component.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 class Invite extends React.Component {
   render() {
     return (
-      <div className="p-3">
-        <h3 className="mb-5" style={{fontSize: '24px'}}>Invite people</h3>
+      <div className="p-4">
+        <h3 className="mb-4" style={{fontSize: '24px'}}>Invite people</h3>
         <div className="field">
           <div className="control has-icons-left">
             <input className="input" type="text"/>
