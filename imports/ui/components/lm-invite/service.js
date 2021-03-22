c02bc92d2e79ac2bddc4cba66d9deb01d9e648eb
webdev0101 <hesimiao2019@gmail.com>
2021-03-23 00:05:37 +0800

    added invite box

--- imports/ui/components/lm-invite/service.js
+++ imports/ui/components/lm-invite/service.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const glDomain = 'http://127.0.0.1:5000'
 
 export const getGuests = async (meetingId) => {
-    const response = await axios.get(`${glDomain}/api/guests?meetingId=${meetingId}`)
+    const response = await axios.get(`${glDomain}/api/guests?uid=${meetingId}`)
     return response.data
 }
 
